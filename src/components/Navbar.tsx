8ff2c2da909c605113c00f06f99c98fa64bbbdc4
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-25 16:50:29 +0100

    forgot to put the tailwindcss setting into the index.css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,33 @@ const Navbar = () => {
     
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-  
+         <ul className='hidden md:flex'>
+        <li>
+          <Link to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to='work' smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
     </div>
   )
 }
