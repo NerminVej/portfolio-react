94ebd4da214a215bbecba3811c4cb1486baa5fa4
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-25 22:14:40 +0100

    finished the about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,17 +1,27 @@
-
 const About = () => {
   return (
-    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
-      <div className='flex flex-col justify-center items-center w-full h-full'>
-        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-          <div className='sm:text-right pb-8 pl-4'>
-            <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
+    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
               Über mich
             </p>
           </div>
           <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-4xl font-bold">
+            <p>Hi. Ich bin Nermin Vejzovic. Seh dich hier gerne um!</p>
           </div>
-        
+          <div>
+            <p>
+              Ich bin leidenschaftlich daran interessiert, ausgezeichnete
+              Software zu entwickeln, die das Leben der Menschen in meiner
+              Umgebung verbessert.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
