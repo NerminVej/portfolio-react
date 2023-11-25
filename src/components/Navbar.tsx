02068b1ef5c2fd10fc431cfb98ff23d3f958ecea
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-25 16:48:31 +0100

    installed react scroll to the project

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+import { Link } from 'react-scroll';
+
+
 
 const Navbar = () => {
+
+    const [nav, setNav] = useState(false);
+    const handleClick = () => setNav(!nav);
+
+    
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-    hi    
+  
     </div>
   )
 }
