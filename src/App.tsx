9b7bca70ad24f1afb8cef862443dd5c18b1a7277
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-25 16:45:53 +0100

    created the navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
     <div>
-Test
+      <Navbar />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
