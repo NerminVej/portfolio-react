1d3f2d73153b12f8e306f10cd590916ba4c2b657
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-26 11:51:46 +0100

    put in the crafted comforts project

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,37 +1,51 @@
 import WorkIm from '../assets/projects/workImg.jpeg'
 import RealEst from '../assets/projects/realestate.jpg'
 import TaskManagement from '../assets/projects/TaskManagementSystem.png'
+import CraftedComforts from '../assets/projects/Craftedcomforts.png'
 
 
 export const data=[
     {
         id:1,
-        name:"Task Management System",
+        name:"Task Management System (React, Java Spring, Tailwind CSS)",
         image:TaskManagement,
         github:"https://github.com/NerminVej/Task-Management",
         live:"",
     },
     {
         id:2,
-        name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
+        name:"Crafted Comforts",
+        image:CraftedComforts,
+        github:"https://github.com/NerminVej/Crafted-Comforts",
+        live:"https://crafted-comforts.vercel.app/",
     },
     {
         id:3,
-        name:"React JS Application",
+        name:"Memories (MERN-Stack)",
         image:WorkIm,
         github:"",
         live:"",
     },
     {
         id:4,
-        name:"React JS Application",
+        name:"Student Managemment System",
         image:RealEst,
         github:"",
         live:"",
     },
-
+    {
+        id:4,
+        name:"Student Managemment System",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+    {
+        id:5,
+        name:"Spotify Clone",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
 
 ]
